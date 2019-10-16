0ca2c31a5ef65c8112674ce529a38d5ae6d17600
Darren Greenfield <darren.greenfield@gmail.com>
2019-10-15 21:51:55 -0700

    fixed bug to run on local environment and removed pug

--- server/app.js
+++ server/app.js
@@ -24,10 +24,6 @@ app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
@@ -41,9 +37,9 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // send the error
   res.status(err.status || 500);
-  res.render('error');
+  res.send(res.locals.message);
 });
 
 app.listen(3001, () => console.log('Listening on port 3001!'));

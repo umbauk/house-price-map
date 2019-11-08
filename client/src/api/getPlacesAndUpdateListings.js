3e850c9f25f3bcbe4a6f7127d433abfe50302c87
Darren Greenfield <darren.greenfield@gmail.com>
2019-11-08 13:21:06 -0800

    Refactor getPlacesAndUpdateListings.js, add limit to area of getPrices request, add code for limiting api requests (not implemented)

--- client/src/api/getPlacesAndUpdateListings.js
+++ client/src/api/getPlacesAndUpdateListings.js
@@ -37,17 +37,15 @@ export async function getPlacesAndUpdateListings(
   return markersArray;
 }
 
+// Looks for properties that are very close together and groups them as duplicates
+// in order to display the details in a single pop-up box
 function deDupeProperties(visiblePropertyDetailsArray) {
   // sort by lat and lng
   visiblePropertyDetailsArray.sort((a, b) => a.lat - b.lat || a.lng - b.lng);
 
+  // Identify if price labels will overlap, if so mark as a duplicate record
   for (let i = 0; i < visiblePropertyDetailsArray.length - 1; i++) {
-    if (
-      visiblePropertyDetailsArray[i].lat <= visiblePropertyDetailsArray[i + 1].lat + 0.00008 &&
-      visiblePropertyDetailsArray[i].lat >= visiblePropertyDetailsArray[i + 1].lat - 0.00008 &&
-      visiblePropertyDetailsArray[i].lng <= visiblePropertyDetailsArray[i + 1].lng + 0.00008 &&
-      visiblePropertyDetailsArray[i].lng >= visiblePropertyDetailsArray[i + 1].lng - 0.00008
-    ) {
+    if (propertiesAreClose(visiblePropertyDetailsArray[i], visiblePropertyDetailsArray[i + 1])) {
       visiblePropertyDetailsArray[i].duplicate = true;
       visiblePropertyDetailsArray[i + 1].duplicate = true;
     }
@@ -57,22 +55,22 @@ function deDupeProperties(visiblePropertyDetailsArray) {
   let duplicateCount = 0;
 
   for (let i = 0; i < visiblePropertyDetailsArray.length - 1; i++) {
-    // if there are duplicates of same property in array, remove them to a new array
+    // if there are duplicates of same or very close property in array, remove them to a new array
     if (visiblePropertyDetailsArray[i].duplicate) {
       let duplicateRowCount = 0;
       duplicatesPropertyDetailsArray[duplicateCount] = [];
       duplicatesPropertyDetailsArray[duplicateCount].push(visiblePropertyDetailsArray[i]);
 
       let j = i + 1;
+      // properties are sorted by lat and lng so if the next property in the array is not close to it
+      // we have found all the duplicates
       while (
-        visiblePropertyDetailsArray[i].lat <= visiblePropertyDetailsArray[j].lat + 0.00008 &&
-        visiblePropertyDetailsArray[i].lat >= visiblePropertyDetailsArray[j].lat - 0.00008 &&
-        visiblePropertyDetailsArray[i].lng <= visiblePropertyDetailsArray[j].lng + 0.00008 &&
-        visiblePropertyDetailsArray[i].lng >= visiblePropertyDetailsArray[j].lng - 0.00008 &&
+        propertiesAreClose(visiblePropertyDetailsArray[i], visiblePropertyDetailsArray[j]) &&
         j < visiblePropertyDetailsArray.length
       ) {
         duplicatesPropertyDetailsArray[duplicateCount].push(visiblePropertyDetailsArray[j]);
         duplicateRowCount++;
+        // if we've reached the end of the array, break the loop
         if (j >= visiblePropertyDetailsArray.length - 1) {
           break;
         } else j++;
@@ -83,9 +81,22 @@ function deDupeProperties(visiblePropertyDetailsArray) {
     }
   }
 
+  // create an array without any duplicates in it
   let deDupedPropertyDetailsArray = visiblePropertyDetailsArray.filter(
     property => !property.duplicate,
   );
 
   return [deDupedPropertyDetailsArray, duplicatesPropertyDetailsArray];
+}
+
+// checks if properties are close together
+function propertiesAreClose(property1, property2) {
+  if (
+    property1.lat <= property2.lat + 0.00008 &&
+    property1.lat >= property2.lat - 0.00008 &&
+    property1.lng <= property2.lng + 0.00008 &&
+    property1.lng >= property2.lng - 0.00008
+  ) {
+    return true;
+  } else return false;
 }
--- server/controllers/houseController.js
+++ server/controllers/houseController.js
@@ -1,4 +1,5 @@
 const House = require('../models/house');
+//const Bottleneck = require('bottleneck');
 
 exports.index = async (req, res, next) => {
   res.send(`Reached index. req = ${req}`);
@@ -7,6 +8,21 @@ exports.index = async (req, res, next) => {
 exports.getPrices = async (req, res, next) => {
   try {
     console.log('getPrices request received');
+
+    // check lat, lng size of request is reasonable and someone isn't trying to pull whole database
+    if (
+      req.params.upperRightLat - req.params.btmLeftLat > 0.016 ||
+      req.params.upperRightLng - req.params.btmLeftLng > 0.027
+    ) {
+      return next('Request area is too large');
+    }
+
+    // set limit on requests of 3 per second
+    /*const limiter = new Bottleneck({
+      minTime: 333,
+    });*/
+
+    //let propertyDetails = await limiter.schedule(() => House.find(
     let propertyDetails = await House.find(
       {
         // only return addresses that are viewable in current map bounds
@@ -20,10 +36,10 @@ exports.getPrices = async (req, res, next) => {
           $gt: req.params.btmLeftLng,
           $lt: req.params.upperRightLng,
         },
-        //postal_code: { $in: ['Dublin 6', 'Dublin 6w'] },
       },
       'lat lng price date_of_sale address',
     );
+    // );
 
     return res.json({ propertyDetails });
   } catch (error) {
